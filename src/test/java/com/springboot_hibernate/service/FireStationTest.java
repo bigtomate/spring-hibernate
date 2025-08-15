53bb7a6b0c2110848d4a30c8ff8d49677552cb02
xing liu <xing.liu@gmx.de>
2025-08-15 18:09:49 +0100

    correct test with throw.

--- src/test/java/com/springboot_hibernate/service/FireStationTest.java
+++ src/test/java/com/springboot_hibernate/service/FireStationTest.java
@@ -1,17 +1,21 @@
 package com.springboot_hibernate.service;
 
+import com.springboot_hibernate.exception.HolidayNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class FireStationTest {
 @Autowired private FireStation fireStationService;
     @Test
     public void extractHolidays() throws IOException {
-        fireStationService.retrieveHolidaysByRegion("");
+        Exception exception = assertThrows(HolidayNotFoundException.class, () -> {
+            fireStationService.retrieveHolidaysByRegion("");
+        });
     }
 }
